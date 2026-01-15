f1e74bc3cdc39497697fb7255364eba5a09e4dd7
Abhishek Mondal <mandal.abhishek2045@gmail.com>
2026-01-15 18:23:08 +0530

    update

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 @app.route("/about")
